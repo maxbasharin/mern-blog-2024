7a228ef1d6b366270ad169d0c3309165332173b6
Max Basharin <maxbasharinweb@gmail.com>
2024-08-26 21:54:43 +0300

    Регистрация и авторизация пользователя с сохранением токена входа

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import { Layout } from './components/Layout'
+import { Layout } from './components/Layout.jsx'
 import { Routes, Route } from 'react-router-dom'
+
 import { MainPage } from './pages/MainPage'
 import { PostsPage } from './pages/PostsPage'
 import { PostPage } from './pages/PostPage'
@@ -7,21 +8,34 @@ import { AddPostPage } from './pages/AddPostPage'
 import { RegisterPage } from './pages/RegisterPage'
 import { LoginPage } from './pages/LoginPage'
 import { EditPostPage } from './pages/EditPostPage'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { getMe } from './redux/features/auth/authSlice.js'
 
 function App() {
-  return (
-    <Layout>
-        <Routes>
-            <Route path='/' element={<MainPage />} />
-            <Route path='posts' element={<PostsPage />} />
-            <Route path=':id' element={<PostPage />} />
-            <Route path=':id/edit' element={<EditPostPage />} />
-            <Route path='new' element={<AddPostPage />} />
-            <Route path='register' element={<RegisterPage />} />
-            <Route path='login' element={<LoginPage />} />
-        </Routes>
-    </Layout>
-)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getMe())
+    }, [dispatch])
+
+    return (
+        <Layout>
+            <Routes>
+                <Route path='/' element={<MainPage />} />
+                <Route path='posts' element={<PostsPage />} />
+                <Route path=':id' element={<PostPage />} />
+                <Route path=':id/edit' element={<EditPostPage />} />
+                <Route path='new' element={<AddPostPage />} />
+                <Route path='register' element={<RegisterPage />} />
+                <Route path='login' element={<LoginPage />} />
+            </Routes>
+
+            <ToastContainer position='bottom-right' />
+        </Layout>
+    )
 }
 
-export default App;
+export default App
